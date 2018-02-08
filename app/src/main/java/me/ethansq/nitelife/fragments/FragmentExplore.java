8aa3d1737d01fa90c35f79333471ba8d474c1765
Ethan Han <ethan.han47@gmail.com>
2018-02-08 03:10:52 -0500

    adding SwipePlaceholderView implementation with sample content

--- app/src/main/java/me/ethansq/nitelife/fragments/FragmentExplore.java
+++ app/src/main/java/me/ethansq/nitelife/fragments/FragmentExplore.java
@@ -8,14 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+
 import me.ethansq.nitelife.R;
+import me.ethansq.nitelife.helpers.Profile;
+import me.ethansq.nitelife.helpers.Utils;
+import me.ethansq.nitelife.views.ProfileCardView;
 
 public class FragmentExplore extends Fragment {
     private final String TAG = "FragmentExplore";
+    private View mView;
+    private SwipePlaceHolderView mSwipeView;
 
-    public FragmentExplore() {
-        // Required empty public constructor
-    }
+    public FragmentExplore() {} // Required empty public constructor
 
     public static FragmentExplore newInstance() {
         FragmentExplore fragment = new FragmentExplore();
@@ -31,7 +37,36 @@ public class FragmentExplore extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        mView = inflater.inflate(R.layout.fragment_explore, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        mSwipeView = (SwipePlaceHolderView) mView.findViewById(R.id.swipeView);
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3);
+//                .setSwipeDecor(new SwipeDecor()
+//                        .setPaddingTop(20)
+//                        .setRelativeScale(0.01f);
+//                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+//                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+
+        for (Profile profile : Utils.loadProfiles(getContext())){
+            mSwipeView.addView(new ProfileCardView(getContext(), profile, mSwipeView));
+        }
+
+//        mView.findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mSwipeView.doSwipe(false);
+//            }
+//        });
+//
+//        mView.findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mSwipeView.doSwipe(true);
+//            }
+//        });
+
+        return mView;
     }
 }
